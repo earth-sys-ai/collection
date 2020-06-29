32867aecc21f708f66c71a175dd591360dc1c51b
Michael Bednarek <mbednarek360@protonmail.com>
2020-06-28 22:26:10 -0400

    WIP storage

--- collect.py
+++ collect.py
@@ -1,6 +1,32 @@
 import geojson
 
 # handles json input
+# sent a feature array
 def handle(raw):
     data = geojson.loads(raw)
-    print(data)
+
+    # scrape uid
+    uid = data["uid"]
+    data.pop("uid")
+
+    # create feature collection
+    fs = geojson.FeatureCollection([data["features"]])
+
+
+    # read from file
+    try:
+        with open(uid + '.json', 'r') as in_file:
+            fs_store = geojson.loads(in_file.read())
+            geojson.FeatureCollection(fs["features"] + fs_store["features"])
+
+    except:
+        print()
+
+    # write collection
+    with open(uid + '.json', 'w') as out_file:
+        geojson.dump(fs, out_file) 
+
+
+# store polygons
+# store image of polygon location
+# assign and sort data by uid
